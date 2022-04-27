900a296a2f1752689293b28e64f719384397da10
zl-php <lei_0668@sina.com>
2022-04-27 16:55:39 +0800

    update code

--- src/LongtuSso.php
+++ src/LongtuSso.php
@@ -1,124 +1,42 @@
 <?php
-/**
- * FILE: Sso.php.
- * User: zhoulei1@longtugame.com
- * Date: 2019/11/27 10:29
- */
-
 namespace Longtugame\Sso;
 
-use GuzzleHttp\Client;
 use Illuminate\Config\Repository;
-use GuzzleHttp\Exception\RequestException;
 
 class LongtuSso {
 
-    /**
-     * sso 配置文件
-     * @var mixed
-     */
-    protected $config     = [];
+    protected $config = [];
 
-    protected $httpClient = null;
-
-    /**
-     * 构造方法
-     */
     public function __construct(Repository $config)
     {
         $this->config = $config->get('sso');
-
-        $this->httpClient = new Client();
     }
 
-    /**
-     * 账号解密
-     *
-     * @param $code
-     * @return \Illuminate\Http\JsonResponse
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
+    // 解密
     public function decrypt($code)
     {
         //解密参数组装
-        $parameter = [
+        $parms = [
             "app_id"      =>  $this->config['app_id'],
             "app_key"     =>  $this->config['app_key'],
             "act"         =>  "decode",
             "code"        =>  $code,
         ];
 
-        $decrypt_post_data = array_merge(['mod' => 'sso', 'signature' => $this->sign($parameter)],$parameter);
+        $data = array_merge(['mod' => 'sso', 'signature' => $this->getSign($parms)], $parms);
 
-        try {
-            $response = $this->post($this->config['url'], $decrypt_post_data);
-        } catch (RequestException $e) {
+        $rsp = json_decode($this->post($this->config['url'], $data), true);
 
-            if ($e->hasResponse())
-                return $this->export(0, $e->getMessage());
-
-            return $this->export(0, $e->getMessage());
+        if (!isset($rsp['rcode']) || $rsp['rcode'] !== 'y050406') {
+            throw new \Exception(
+                'get result error:'.$rsp['rcode'].' - '.$rsp['msg']
+            );
         }
 
-        $result = mb_convert_encoding($response->getBody(), 'utf-8', 'gb2312');
-
-        $result = json_decode($result, true);
-
-        if($result['rcode'] != 'y050406' || empty($result['code']))
-            return $this->export(0, '解密失败或参数被篡改');
-
-        return $this->export(1, '解密成功', $result['code']);
+        return $rsp;
     }
 
-    /**
-     * 绑定账号到应用
-     *
-     * @param $email
-     * @return \Illuminate\Http\JsonResponse
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    public function bind($email)
-    {
-        //绑定sso参数组装
-        $parameter = [
-            "app_id"    =>  $this->config['app_id'],
-            "app_key"   =>  $this->config['app_key'],
-            "act"       =>  "bind",
-            'user_mail' =>  $email,
-            "app_identify_code" =>  json_encode([[ 'uname_inapp' => $this->config['app_id'],'identify_inapp' => encrypt($email)]])
-        ];
-
-        $bind_get_data = array_merge(['mod' => 'sso', 'signature' => $this->sign($parameter)],$parameter);
-
-        try {
-
-            $response = $this->get($this->config['url'], $bind_get_data);
-
-        } catch (RequestException $e) {
-
-            if ($e->hasResponse())
-                return $this->export(0, $e->getMessage());
-
-            return $this->export(0, $e->getMessage());
-        }
-
-        $result = mb_convert_encoding($response->getBody(), 'utf-8', 'gb2312');
-
-        $result = json_decode($result, true);
-
-        if($result['rcode'] != 'y130101')
-            return $this->export(0, $result['msg']);
-
-        return $this->export(1, $result['msg']);
-    }
-
-    /**
-     * 参数签名
-     *
-     * @param array $arr_params
-     * @return string
-     */
-    private function sign($arr_params = [])
+    private function getSign($arr_params = [])
     {
         ksort($arr_params);
         reset($arr_params);
@@ -131,53 +49,12 @@ class LongtuSso {
         return md5($_str_signSrc);
     }
 
-    /**
-     * get请求
-     * @param $endpoint
-     * @param array $query
-     * @param array $headers
-     * @return mixed|\Psr\Http\Message\ResponseInterface
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    private function get($endpoint, $query = [], $headers = [])
+    private function post($url, $params = [], $headers = [])
     {
-        return $this->httpClient->request('get', $endpoint, [
-            'headers' => $headers,
-            'query'   => $query,
-        ]);
+        $client   = new \GuzzleHttp\Client();
+
+        $rsp = $client->request('POST', $url, ['headers' => $headers, 'form_params' => $params, 'http_errors' => false]);
+
+        return $rsp->getBody()->getContents();
     }
-
-    /**
-     * post请求
-     *
-     * @param $endpoint
-     * @param $data
-     * @param array $options
-     * @return mixed|\Psr\Http\Message\ResponseInterface
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    private function post($endpoint, $data, $options = [])
-    {
-        if (!is_array($data)) {
-            $options['body'] = $data;
-        } else {
-            $options['form_params'] = $data;
-        }
-
-        return $this->httpClient->request('post', $endpoint, $options);
-    }
-
-    /**
-     * 返回数据
-     *
-     * @param $code
-     * @param $message
-     * @param array $data
-     * @return \Illuminate\Http\JsonResponse
-     */
-    private function export($code, $message, $data = [])
-    {
-        return response()->json(['code' => $code, 'message' => $message, 'data' => $data]);
-    }
-
 }
